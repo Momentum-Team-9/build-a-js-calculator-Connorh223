c30faf65cb560f2b166575f2b9e195385071575b
Connor Hickman <Connorh223@gmail.com>
2021-07-14 18:33:43 -0400

    add main.css

--- main.js
+++ main.js
@@ -5,5 +5,86 @@ console.log(num1)
 num1.addEventListener('click', function (event) {
   console.log(event.target.innerText)
   console.log('button clicked')
-  display.innerText = event.target.innerText
+  display.innerText += event.target.innerText
+})
+
+const num0 = document.getElementById('num0')
+console.log(num0)
+
+num0.addEventListener('click', function (event) {
+  display.innerText += event.target.innerText
+  console.log(event.target.innerText)
+  console.log('button clicked')
+})
+
+const num2 = document.getElementById('num2')
+console.log(num2)
+
+num2.addEventListener('click', function (event) {
+  display.innerText += event.target.innerText
+  console.log(event.target.innerText)
+  console.log('button clicked')
+})
+
+const num3 = document.getElementById('num3')
+console.log(num3)
+
+num3.addEventListener('click', function (event) {
+  display.innerText += event.target.innerText
+  console.log(event.target.innerText)
+  console.log('button clicked')
+})
+
+const num4 = document.getElementById('num4')
+console.log(num4)
+
+num4.addEventListener('click', function (event) {
+  display.innerText += event.target.innerText
+  console.log(event.target.innerText)
+  console.log('button clicked')
+})
+
+const num5 = document.getElementById('num5')
+console.log(num5)
+
+num5.addEventListener('click', function (event) {
+  display.innerText += event.target.innerText
+  console.log(event.target.innerText)
+  console.log('button clicked')
+})
+
+const num6 = document.getElementById('num6')
+console.log(num6)
+
+num6.addEventListener('click', function (event) {
+  display.innerText += event.target.innerText
+  console.log(event.target.innerText)
+  console.log('button clicked')
+})
+
+const num7 = document.getElementById('num7')
+console.log(num7)
+
+num7.addEventListener('click', function (event) {
+  display.innerText += event.target.innerText
+  console.log(event.target.innerText)
+  console.log('button clicked')
+})
+
+const num8 = document.getElementById('num8')
+console.log(num8)
+
+num8.addEventListener('click', function (event) {
+  display.innerText += event.target.innerText
+  console.log(event.target.innerText)
+  console.log('button clicked')
+})
+
+const num9 = document.getElementById('num9')
+console.log(num9)
+
+num9.addEventListener('click', function (event) {
+  display.innerText += event.target.innerText
+  console.log(event.target.innerText)
+  console.log('button clicked')
 })
